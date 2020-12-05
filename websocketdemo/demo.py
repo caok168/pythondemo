d2d4629f88537b03eb02f68650437c0651e45a8f
caokai168@hotmail.com <caokai168@hotmail.com>
2020-12-05 20:44:51 +0800

    websocket demo

---
+++ websocketdemo/demo.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sockets import Sockets
+import datetime
+import time
+import random
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        now = datetime.datetime.now().isoformat() + 'Z'
+        ws.send(now)
+        time.sleep(1)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('',5000),app,handler_class=WebSocketHandler)
+    print('server start')
+    server.server_forever()
